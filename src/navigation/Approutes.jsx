b19f4e8fb29f26fff856d01d82666f25677acb77
RobUXUI <roberto.munizaga@bootcampudd.cl>
2024-01-18 22:47:25 -0300

    fixed error deploy

--- src/navigation/Approutes.jsx
+++ src/navigation/Approutes.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import Home from '../views/Home/Home';
 import Nosotros from '../views/Nosotros/Nosotros';
 import Menu from '../views/Menu/Menu';
 import Reservas from '../views/Reservas/Reservas';
-import Home from '../views/Home/Home';
 import Layout from '../layout/layout';
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/nosotros" element={<Nosotros />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/reservas" element={<Reservas />} />
-      </Route>
+      <Route path="/" element={<Layout />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/nosotros" element={<Nosotros />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/reservas" element={<Reservas />} />
     </Routes>
   );
 }
