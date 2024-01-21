fbdcda051245012bccddc7144bf64754451810a5
RobUXUI <roberto.munizaga@bootcampudd.cl>
2024-01-21 17:13:48 -0300

    error fixed

--- src/navigation/Approutes.jsx
+++ src/navigation/Approutes.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../componentes/layout/Layout';
-import Home from '../views/home/Home';
-import Nosotros from '../views/nosotros/Nosotros';
-import Menu from '../views/menu/Menu';
-import Reservas from '../views/reservas/Reservas';
+import Home from '../views/home/Home.jsx';
+import Nosotros from '../views/nosotros/Nosotros.jsx';
+import Menu from '../views/menu/Menu.jsx';
+import Reservas from '../views/reservas/Reservas.jsx';
 
 function AppRoutes() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Layout/>}>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="nosotros" element={<Nosotros />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="reservas" element={<Reservas />} />
+          <Route index element={<Home/>} />
+          <Route path="home" element={<Home/>} />
+          <Route path="nosotros" element={<Nosotros/>} />
+          <Route path="menu" element={<Menu/>} />
+          <Route path="reservas" element={<Reservas/>} />
         </Route>
       </Routes>
     </div>
